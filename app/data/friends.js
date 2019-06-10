8efdd1b7a4c9e4bc47cdb30256e38ddd34099e4c
smprov22 <smprovence22@gmail.com>
2019-06-09 22:14:20 -0700

    routes and server working

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friendArray = [
+    {
+      name: "Stephanie",
+      photoURL: "ahmed@example.com",
+      scores: [5,2,3,2,1,4,2,5,1,3]
+    }
+  ];
+
+  module.exports = friendArray;
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var path = require("path");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
