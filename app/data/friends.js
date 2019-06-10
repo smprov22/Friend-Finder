e0644fe7af026fb1271239468af08d21def50ae1
smprov22 <smprovence22@gmail.com>
2019-06-10 15:08:35 -0700

    working minus modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,53 +1,53 @@
 var friendArray = [
     {
-      name: "Morgan Brian",
-      photoURL: "https://pbs.twimg.com/profile_images/1071603145258733569/bomCxLpd_400x400.jpg",
-      scores: [5,2,3,2,1,4,2,5,1,3]
+        name: "Morgan Brian",
+        photoURL: "https://pbs.twimg.com/profile_images/1071603145258733569/bomCxLpd_400x400.jpg",
+        answers: [5,2,3,2,1,4,2,5,1,3]
     },
     {
         name: "Crystal Dunn",
         photoURL: "https://cdn2.sportngin.com/attachments/photo/1008/6903/NWSLBS03162018201_large.jpg",
-        scores: [2,4,5,1,3,2,3,5,1,2]
+        answers: [2,4,5,1,3,2,3,5,1,2]
     },
     {
         name: "Julie Ertz",
         photoURL: "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/wnt/julie-ertz/julieertz.ashx?h=580&la=en-US&w=580&rev=0199d49cefe9428482333e40a8c1b39c&hash=628C741D2D48DEAFEDD420D20B04A67B",
-        scores: [3,1,2,4,1,5,2,3,3,1]
+        answers: [3,1,2,4,1,5,2,3,3,1]
     },
     {
         name: "Tobin Heath",
         photoURL: "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/wnt/tobin-heath/tobinheath.ashx?h=580&la=en-US&w=580&rev=2f2e3af27d8b4e93b891e48e9159dd0f&hash=8E1D572E3694C9557790E32FB5E4E594",
-        scores: [1,2,4,5,3,1,2,5,4,4]
+        answers: [1,2,4,5,3,1,2,5,4,4]
     },
     {
         name: "Alex Morgan",
         photoURL: "https://www.google.com/search?biw=1294&bih=690&tbm=isch&sa=1&ei=OO79XN7_LpHr-gSUkI-gCQ&q=alex+morgan&oq=alex+morgan&gs_l=img.3..0i67l2j0l4j0i67j0j0i67j0.89185.94192..95152...0.0..2.98.1166.18......0....1..gws-wiz-img.....0..35i39.PHy3hWvYeHw#imgrc=YwbMqwHfiKcn1M:",
-        scores: [3,1,5,2,2,1,1,4,3,5]
+        answers: [3,1,5,2,2,1,1,4,3,5]
     },
     {
         name: "Kelley O'Hara",
         photoURL: "https://cdn-s3.si.com/s3fs-public/images/Kelley-ohara-X159570_TK2_1123.jpg",
-        scores: [4,3,5,1,2,3,5,5,4,2]
+        answers: [4,3,5,1,2,3,5,5,4,2]
     },
     {
         name: "Christen Press",
         photoURL: "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/wnt/christen-press/christenpress.ashx?h=580&la=en-US&w=580&rev=d6db250c167045b9a296ac13032ba714&hash=29BFEF048671090CF84B7D1293C0E28A",
-        scores: [5,1,1,2,3,4,5,2,3,4]
+        answers: [5,1,1,2,3,4,5,2,3,4]
     },
     {
         name: "Mallory Pugh",
         photoURL: "http://d2e5urt2vl2d1u.cloudfront.net/wp-content/uploads/2016/08/08124833/9.jpg",
-        scores: [4,3,4,4,1,2,5,5,3,2]
+        answers: [4,3,4,4,1,2,5,5,3,2]
     },
     {
         name: "Megan Rapinoe",
         photoURL: "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/wnt/megan-rapinoe/meganrapinoe.ashx?h=580&la=en-US&w=580&rev=ab6d97eadbf94433a198ea5f1005e5b1&hash=719BD44A4828D8E2496AFC44F17AE80F",
-        scores: [5,3,1,2,4,2,4,5,1,3]
+        answers: [5,3,1,2,4,2,4,5,1,3]
     },
     {
         name: "Carli LLoyd",
         photoURL: "http://www.carlilloyd.com/wp-content/uploads/2017/06/carli-bio.png",
-        scores: [2,5,1,3,4,1,2,3,1,5]
+        answers: [2,5,1,3,4,1,2,3,1,5]
     }
   ];
 
